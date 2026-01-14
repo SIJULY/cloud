b9bc4b220b40c704ea32ee22932b4255f2373555
SIJULY <104404467+SIJULY@users.noreply.github.com>
2026-01-15 02:15:08 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+
 # 启动 Celery Worker (后台运行)
 celery -A tasks worker --loglevel=info &
 
-# 启动 Flask (使用 Gunicorn 生产级服务器)
-# -w 4: 4个工作进程
+# 启动 Flask (使用 Gunicorn)
+# -w 4: 启动 4 个工作进程 (占用内存的主力)
+# --threads 10: 每个进程开启 10 个线程 (处理并发的主力，处理上传下载非常有效)
 # -b 0.0.0.0:5000: 监听端口
-exec gunicorn -w 4 -b 0.0.0.0:5000 app:app
+# --timeout 0: 【关键】设置为 0 代表永不超时，直到上传完成
+exec gunicorn -w 4 --threads 10 -b 0.0.0.0:5000 --timeout 0 app:app
